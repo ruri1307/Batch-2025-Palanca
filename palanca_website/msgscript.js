2c501ed6af52cbd4dd4b425fe13379b19d8adce0
ruri1307 <161279924+ruri1307@users.noreply.github.com>
2025-03-26 13:09:32 +0800

    syntax and naming modifications

--- palanca_website/msgscript.js
+++ palanca_website/msgscript.js
@@ -2,4 +2,4 @@ const graduationDate = new Date('2025-05-26T00:00:00');
 currentDate = Date.now();
 timeTillGrad = Math.round((graduationDate-currentDate)/(1000 * 60 * 60 * 24));
 
-document.getElementById('counter').innerText = (String(timeTillGrad) + ' days...');
+document.getElementById('days').innerText = (String(timeTillGrad) + ' days...');
