e5e69ea166316ea9c1e439d17d976af0fd0e2b39
ruri1307 <161279924+ruri1307@users.noreply.github.com>
2025-03-26 08:12:35 +0800

    resolved syntax in javascript files

--- palanca_website/script.js
+++ palanca_website/script.js
@@ -1,6 +1,6 @@
 const graduationDate = new Date('2025-05-26T00:00:00');
-currentDate = Date.now()
-timeTillGrad = Math.round((graduationDate-currentDate)/(1000 * 60 * 60 * 24))
+currentDate = Date.now();
+timeTillGrad = Math.round((graduationDate-currentDate)/(1000 * 60 * 60 * 24));
 
 document.getElementById('counter').innerText = (String(timeTillGrad) + ' days...');
 
