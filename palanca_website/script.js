6484af81d24945684fd2f6dd0ac1babc3378f445
ruri1307 <161279924+ruri1307@users.noreply.github.com>
2025-03-26 08:05:58 +0800

    added styling to main html and started styling on msg html

--- palanca_website/script.js
+++ palanca_website/script.js
@@ -1,3 +1,9 @@
+const graduationDate = new Date('2025-05-26T00:00:00');
+currentDate = Date.now()
+timeTillGrad = Math.round((graduationDate-currentDate)/(1000 * 60 * 60 * 24))
+
+document.getElementById('counter').innerText = (String(timeTillGrad) + ' days...');
+
 const cardlist = document.getElementsByClassName('card');
 const namelist = [
     'ALBARRACIN KARL GABRIEL D.',
